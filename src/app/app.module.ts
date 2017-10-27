8fefec15cb754a0ad9c092e9a5789bfc40d16bce
srikanthr <srikanthr@gratex.com.au>
2017-10-27 16:23:03 +1100

    some improvements added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 //Custom modules
 import { BraintreeModule } from './braintree/braintree.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     BraintreeModule
   ],
   providers: [],
