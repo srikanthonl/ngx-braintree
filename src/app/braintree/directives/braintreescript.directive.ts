1e9167010e6938322cb5488666e6eb039ff9823e
srikanthonl <srikanthreddyone@gmail.com>
2017-11-15 21:41:56 +1100

    now uses Renderer2

--- src/app/braintree/directives/braintreescript.directive.ts
+++ src/app/braintree/directives/braintreescript.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, OnInit, OnDestroy } from '@angular/core';
+import { Directive, OnInit, OnDestroy, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Directive({
   selector: '[appBraintreescript]'
@@ -7,17 +8,16 @@ export class BraintreescriptDirective implements OnInit, OnDestroy {
 
   script: any;
 
-  constructor() {
-  }
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
-    this.script = document.createElement('script');
+    this.script = this.renderer.createElement('script');
     this.script.type = 'text/javascript';
     this.script.src = 'https://js.braintreegateway.com/web/dropin/1.8.0/js/dropin.min.js';
-    document.getElementsByTagName('body')[0].appendChild(this.script);
+    this.renderer.appendChild(this.document.body, this.script);
   }
 
   ngOnDestroy() {
-    document.getElementsByTagName('body')[0].removeChild(this.script);
+    this.renderer.removeChild(this.document.body, this.script);
   }
 }
