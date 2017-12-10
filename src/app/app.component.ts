3c6cf0115787f6551c74d006cfabdb2990088538
srikanthonl <srikanth@srikanth.onl>
2017-12-10 23:16:13 +1100

    few improvements related to the way nonce is posted to the server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  paymentResponse: any;
 
   onPaymentStatus(response): void {
-    console.log(response);
+    this.paymentResponse = response;
   }
 }
