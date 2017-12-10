f4a44d4e35e415cc1c7798b30b7dafdca8388b44
srikanthonl <srikanth@srikanth.onl>
2017-12-10 23:38:19 +1100

    unnecessary code removal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
   paymentResponse: any;
 
   onPaymentStatus(response): void {
