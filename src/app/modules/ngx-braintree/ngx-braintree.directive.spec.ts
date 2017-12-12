e302f06f7dbe95cd15a2e66b2f631439ea07fd16
srikanthr <srikanthr@gratex.com.au>
2017-12-12 11:25:20 +1100

    clean up

--- src/app/modules/ngx-braintree/ngx-braintree.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NgxBraintreeDirective } from './ngx-braintree.directive';
-
-describe('NgxBraintreeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgxBraintreeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
