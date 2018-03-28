035258a89166f72b921c1f0190372b2fb7e0f948
srikanthr <srikanthr@gratex.com.au>
2018-03-28 15:17:13 +1100

    few changes related to 46

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +14,54 @@ export class AppComponent {
   paymentResponse: any;
   chargeAmount = 55.55;
 
+  constructor(private http: HttpClient) { }
+
   onPaymentStatus(response): void {
     this.paymentResponse = response;
   }
+
+  // ES 5 Function
+  getClientToken(): Observable<string> {
+    return this.http
+      .get('api/braintree/getclienttoken', { responseType: 'json' })
+      .map((response: any) => {
+        return response.token;
+      })
+      .catch((error) => {
+        return Observable.throw(error);
+      });
+  }
+
+  // ES 5 Function
+  createPurchase(nonce: string, chargeAmount: number): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http
+    .post('api/braintree/createpurchase', { nonce: nonce, chargeAmount: chargeAmount }, { 'headers': headers })
+      .map((response: any) => {
+        return response;
+      });
+  }
+
+  // ES 2015 Function
+  // getClientToken = () => {
+  //   return this.http
+  //     .get('api/braintree/getclienttoken', { responseType: 'json' })
+  //     .map((response: any) => {
+  //       return response.token;
+  //     })
+  //     .catch((error) => {
+  //       return Observable.throw(error);
+  //     });
+  // }
+
+  // ES 2015 Function
+  // createPurchase = (nonce: string, chargeAmount: number) => {
+  //   const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  //   return this.http
+  //     .post('api/braintree/createpurchase', { nonce: nonce, chargeAmount: chargeAmount }, { 'headers': headers })
+  //     .map((response: any) => {
+  //       return response;
+  //     });
+  // }
+
 }
