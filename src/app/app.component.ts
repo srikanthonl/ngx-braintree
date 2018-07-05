6c1ac39544e61d0ce5cbf0a953d17a7d4311ca3a
srikanthr <srikanthr@gratex.com.au>
2018-07-05 14:12:57 +1000

    issue 59 related development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,37 +6,37 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   paymentResponse: any;
   chargeAmount = 55.55;
 
   enabledStyle = {
-    'background-color': '#000000',
-    'color': '#ffffff',
-    'border': 'none',
-    'border-radius': '4px',
-    'height': '40px',
-    'line-height': '40px',
-    'font-size': '16px',
-    'cursor': 'pointer'
+    "background-color": "#000000",
+    color: "#ffffff",
+    border: "none",
+    "border-radius": "4px",
+    height: "40px",
+    "line-height": "40px",
+    "font-size": "16px",
+    cursor: "pointer"
   };
 
   disabledStyle = {
-    'background-color': 'lightgray',
-    'color': '#ffffff',
-    'border': 'none',
-    'border-radius': '4px',
-    'height': '40px',
-    'line-height': '40px',
-    'font-size': '16px',
-    'cursor': 'not-allowed'
+    "background-color": "lightgray",
+    color: "#ffffff",
+    border: "none",
+    "border-radius": "4px",
+    height: "40px",
+    "line-height": "40px",
+    "font-size": "16px",
+    cursor: "not-allowed"
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   onPaymentStatus(response): void {
     this.paymentResponse = response;
@@ -67,27 +67,34 @@ export class AppComponent {
   // ES 2015 Function
   getClientToken = () => {
     return this.http
-      .get('api/braintree/getclienttoken', { responseType: 'json' })
+      .get("api/braintree/getclienttoken", { responseType: "json" })
       .map((response: any) => {
         return response.token;
       })
-      .catch((error) => {
+      .catch(error => {
         return Observable.throw(error);
       });
-  }
+  };
 
   // ES 2015 Function
   createPurchase = (nonce: string, chargeAmount: number) => {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const headers = new HttpHeaders({ "Content-Type": "application/json" });
     return this.http
-      .post('api/braintree/createpurchase', { nonce: nonce, chargeAmount: chargeAmount }, { 'headers': headers })
+      .post(
+        "api/braintree/createpurchase",
+        { nonce: nonce, chargeAmount: chargeAmount },
+        { headers: headers }
+      )
       .map((response: any) => {
         return response;
       });
+  };
+
+  OnDropInLoaded(event) { 
+    console.log('dropin loaded...');
   }
 
-  navigateToPreviousPage() { 
-    console.log('navigate to your previous page here...');
+  navigateToPreviousPage() {
+    console.log("navigate to your previous page here...");
   }
-
 }
